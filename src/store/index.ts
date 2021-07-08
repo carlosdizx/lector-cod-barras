563bac9613b57a7b2879b4401c33bb7ca7c6cf02
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-08 18:48:40 -0500

    Store preparado para agregar productos a una variable

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    productos:[{}]
   },
   mutations: {
+    addProducto(state, payload){
+      state.productos.push(payload);
+    }
   },
   actions: {
+    agregarProducto({commit},data){
+      commit('addProducto',data)
+    }
   },
   modules: {
   }
