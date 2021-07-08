3c55f352781bb6263d284139d93bd3fcd1bf51ac
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-08 17:03:52 -0500

    Terminando pruebas

--- src/views/Escaneo.vue
+++ src/views/Escaneo.vue
@@ -1,12 +1,33 @@
 <template>
+  <svg id="barcode"></svg>
 </template>
-
 <script>
+const JsBarcode = require('jsbarcode');
+const { Canvas } = require("canvas");
+
 export default {
-  name: "Escaneo"
+  name: "Escaneo",
+  data:()=>({
+    producto:{
+      id: 10,
+      nombre:'Cafe con leche',
+      precio:5000
+    }
+  }),
+  methods:{
+    convertirString(){
+      return this.producto.nombre+'||'+this.producto.precio
+    }
+  },
+  mounted() {
+    console.log(JSON.stringify(this.producto))
+    JsBarcode("#barcode", this.convertirString(), {
+      text: this.producto.nombre,
+      background: "#FFF",
+      lineColor: "#000",
+      font: "fantasy",
+    });
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
