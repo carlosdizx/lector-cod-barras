dfe8b2dc87cc2f6d3ab8e3e59df112729cb9b68c
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-08 19:07:21 -0500

    Creada un componente generico para la creacion de tablas dinamicas

---
+++ src/components/general/Tabla.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-card-title>{{ titulo }}</v-card-title>
+    <v-data-table
+        :headers="columnas"
+        :items="lista"
+        :items-per-page="5"
+        :footer-props="{
+				showFirstLastPage: true,
+				firstIcon: 'mdi-arrow-collapse-left',
+				lastIcon: 'mdi-arrow-collapse-right',
+				prevIcon: 'mdi-minus',
+				nextIcon: 'mdi-plus',
+				rowsPerPage:'8'
+			}"
+        class="elevation-1"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tabla",
+  props: {
+    titulo: '',
+    columnas: [],
+    lista: [],
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
