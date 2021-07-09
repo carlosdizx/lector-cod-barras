f830c3eeadaff907edf4daff7daaa36fda41e3ec
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-08 19:27:24 -0500

    Listo el listado, ahora hay que agregar los codigos de barras

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,44 @@
 <template>
+  <v-container>
+    <hr>
+    <v-btn @click="agregar">Agregar producto</v-btn>
+    <router-link to="escaneo" v-slot="{ navigate }" custom>
+      <v-btn role="link" @click="navigate">Ver codigos de barras</v-btn>
+    </router-link>
+    <hr>
+    <Tabla titulo="Listado de productos" :columnas="columnas" :lista="productos" />
+  </v-container>
 </template>
 
-<script lang="ts">
+<script>
   import Vue from 'vue'
-
+  import Tabla from '../components/general/Tabla'
+  import {mapActions, mapState} from 'vuex'
   export default Vue.extend({
     name: 'Home',
-
     components: {
+      Tabla
     },
+    data: () => ({
+      columnas: [
+        { text: 'Nombre', value: 'nombre' },
+        { text: 'Precio Compra', value: 'precio' },
+        { text: '||--', value: 'codigo' },
+      ],
+      lista :[]
+    }),
+    computed:{
+      ...mapState(['productos'])
+    },
+    mounted() {
+      this.lista = this.productos;
+      console.log(JSON.stringify(this.productos))
+    },
+    methods:{
+      ...mapActions(['agregarProducto']),
+      agregar(){
+        this.agregarProducto({nombre:'lo que sea',precio:5000,codigo:Math.random() * (10000 - 1) + 1})
+      }
+    }
   })
 </script>
